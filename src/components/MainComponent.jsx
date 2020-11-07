23e05dc949688952f832cf40227e10468c26c764
Ali Kalout <63719442+alikk351@users.noreply.github.com>
2020-11-07 16:24:47 +0200

    SPA

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,18 +1,29 @@
 import Menu from "./Menu"
-import DISHES from "./../shared/dishes";
 import { useState } from 'react';
+import DISHES from "./../shared/dishes";
 import DishDetail from "./DishdetailComponent";
 import Header from "./Header";
 import Footer from "./Footer";
 import Home from "./Home";
+import Contact from "./Contact";
+import { COMMENTS } from "./../shared/comments";
+import { LEADERS } from "./../shared/leaders";
+import { PROMOTIONS } from "./../shared/promotions";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 function Main(props) {
     const [dishes, changeDished] = useState(DISHES);
+    const [leaders, changeLeaders] = useState(LEADERS);
+    const [comments, changeComments] = useState(COMMENTS);
+    const [promotions, changePromotions] = useState(PROMOTIONS);
 
-    const HomePage = ()=>{
+    const HomePage = () => {
         return (
-            <Home />
+            <Home
+                dish={dishes.filter(d => d.featured === true)[0]}  // d for one dish
+                leader={leaders.filter(l => l.featured === true)[0]}
+                promotion={promotions.filter(promo => promo.featured === true)[0]}
+            />
         );
     }
 
@@ -24,9 +35,13 @@ function Main(props) {
             <Switch>
 
                 <Route path="/home" component={HomePage} />
+
                 <Route exact path="/menu" component={() => <Menu dishz={dishes} />} />
                 {/* exact cuz we will use '/menu/shi' for dish details */}
-                <Redirect to="/home" /> {/* for default route redirect to home */}
+
+                <Route path="/contactus" component={Contact} />
+
+                <Redirect to="/home" /> {/* for default route redirect to home === should be last*/}
 
             </Switch>
 
