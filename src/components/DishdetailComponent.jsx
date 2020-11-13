0bd5249838aae4b26530b58c78c5b26c6b699703
Ali Kalout <63719442+alikk351@users.noreply.github.com>
2020-11-13 16:07:11 +0200

    meshe l 7al

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -54,6 +54,52 @@ function CommentForm() {
     );
 }
 
+function DishdetailComponent({ any_dish }) {   // use it in MERN-auth to display a modal of a post
+    if (any_dish === null) {
+        return (<div></div>);
+    } else {
+        return (
+            <Card >
+                <CardImg width="100%" src={any_dish.image} alt={any_dish.name} />
+                <CardBody>
+                    <CardTitle tag="h5">{any_dish.name}</CardTitle>
+                    <CardText>{any_dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    }
+}
+
+function RenderComments({ c }) {
+    console.log("Render invoked");
+    if (c === null) {
+        return (<div></div>);
+    } else {
+        return (
+            <div>
+                <h5>Comments</h5>
+                <br></br>
+                <ul className="list-unstyled">
+                    {c.map((cmnt, i) => {
+                        return (
+                            <div key={i}>
+                                <li>{cmnt.comment}</li>
+                                <li>-- {cmnt.author} , {new Intl.DateTimeFormat("en-US", {
+                                    year: "numeric",
+                                    month: "short",
+                                    day: "2-digit"
+                                }).format(new Date(Date.parse(cmnt.date)))}</li>
+                                <br></br>
+                            </div>
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
+
+
 function DishDetail(props) {
     const dish = props.a_dish;
     const comments = props.cmnts;
@@ -61,51 +107,6 @@ function DishDetail(props) {
     React.useEffect(() => {
         console.log("DishDetail invoked");
     });
-
-    function DishdetailComponent(any_dish) {   // use it in MERN-auth to display a modal of a post
-        if (any_dish === null) {
-            return (<div></div>);
-        } else {
-            return (
-                <Card >
-                    <CardImg width="100%" src={any_dish.image} alt={any_dish.name} />
-                    <CardBody>
-                        <CardTitle tag="h5">{any_dish.name}</CardTitle>
-                        <CardText>{any_dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-    }
-
-    function renderComments(c) {
-        console.log("Render invoked");
-        if (c === null) {
-            return (<div></div>);
-        } else {
-            return (
-                <div>
-                    <h5>Comments</h5>
-                    <br></br>
-                    <ul className="list-unstyled">
-                        {c.map((cmnt, i) => {
-                            return (
-                                <div key={i}>
-                                    <li>{cmnt.comment}</li>
-                                    <li>-- {cmnt.author} , {new Intl.DateTimeFormat("en-US", {
-                                        year: "numeric",
-                                        month: "short",
-                                        day: "2-digit"
-                                    }).format(new Date(Date.parse(cmnt.date)))}</li>
-                                    <br></br>
-                                </div>
-                            );
-                        })}
-                    </ul>
-                </div>
-            );
-        }
-    }
 
     return (
         <div className="container">
@@ -121,10 +122,10 @@ function DishDetail(props) {
             </div>
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
-                    {DishdetailComponent(dish)}
+                    <DishdetailComponent any_dish={props.a_dish} />
                 </div>
                 <div className="col-12 col-md-5 m-1">
-                    {renderComments(comments)}
+                    <RenderComments c={props.cmnts} />
                     <CommentForm />
                 </div>
             </div>
